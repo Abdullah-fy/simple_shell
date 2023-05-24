051a91c0e51f1bebec9085e5d4110ddf777f96fa
Abdullah-fy <https://github.com/Abdullah-fy/simple_shell.git>
2023-05-24 21:04:14 +0300

    d

--- cd_file.c
+++ cd_file.c
@@ -6,8 +6,33 @@
  *
  * Return: 1 one success, 0 otherwise.
  */
+#include <stdio.h>
+#include <unistd.h>
+
 int cd_file(char **arg)
 {
-	chdir(arg[1]);
-	return (0);
+    char cwd[4096];
+
+    if (arg[1] == NULL || strcmp(arg[1], "~") == 0)
+    {
+        chdir(getenv("HOME"));
+    }
+    else if (strcmp(arg[1], "-") == 0)
+    {
+        chdir(getenv("OLDPWD"));
+    }
+    else
+    {
+        if (chdir(arg[1]) == -1)
+        {
+            perror("cd");
+        }
+    }
+
+    getcwd(cwd, sizeof(cwd));
+    setenv("PWD", cwd, 1);
+    setenv("OLDPWD", getenv("PWD"), 1);
+
+    return 0;
 }
+
